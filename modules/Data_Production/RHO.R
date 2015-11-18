9c14b12abccdbf675cbb64d023239171dfd81a49
droglenc <dogle@northland.edu>
2015-11-18 17:50:35 -0600

    again

---
+++ modules/Data_Production/RHO.R
@@ -0,0 +1,20 @@
+# Renders an appropriate HTML file for the webpage
+setwd("C:/aaaWork/Web/GitHub/NCMTH107/modules/Data_Production")
+source("../../rhelpers/rhelpers.R")
+fnm <- "RHO"
+modHTML(fnm)
+
+library(NCStats)
+fnm <- "RHO"
+source("../../rhelpers/knitr_setup.R")
+set.seed(11545345)
+
+sample(736,10)
+
+( alloc <- sample(24) )
+alloc[1:8]
+alloc[9:16]
+alloc[17:24]
+
+
+# Script created at 2015-11-18 17:50:29
