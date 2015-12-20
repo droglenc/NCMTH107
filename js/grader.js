b5d10447af9efc31c47e1ded18d8425125b5bf45
droglenc <dogle@northland.edu>
2015-12-20 11:33:57 -0600

    Updated grader

--- js/grader.js
+++ js/grader.js
@@ -21,17 +21,17 @@ function get_wghts() {
 //**  Computes the final grade                                                    **
 //**********************************************************************************
 function find_grade(form, pts, outof) {
-	curr_sc = pts/outof;
-	form.SCORE.value = roundDec(curr_sc*100,1);
-	if (curr_sc > 0.92) {form.GRADE.value="A"}
-	else if (curr_sc > 0.90) {form.GRADE.value="A-"}
-	else if (curr_sc > 0.87) {form.GRADE.value="B+"}
-	else if (curr_sc > 0.82) {form.GRADE.value="B"}
-	else if (curr_sc > 0.80) {form.GRADE.value="B-"}
-	else if (curr_sc > 0.77) {form.GRADE.value="C+"}
-	else if (curr_sc > 0.70) {form.GRADE.value="C"}
-	else if (curr_sc > 0.67) {form.GRADE.value="D+"}
-	else if (curr_sc > 0.60) {form.GRADE.value="D"}
+	form.SCORE.value = roundDec((pts/outof)*100,1);
+	curr_sc = roundDec(pts/outof,2);
+	if (curr_sc >= 0.92) {form.GRADE.value="A"}
+	else if (curr_sc >= 0.90) {form.GRADE.value="A-"}
+	else if (curr_sc >= 0.87) {form.GRADE.value="B+"}
+	else if (curr_sc >= 0.82) {form.GRADE.value="B"}
+	else if (curr_sc >= 0.80) {form.GRADE.value="B-"}
+	else if (curr_sc >= 0.77) {form.GRADE.value="C+"}
+	else if (curr_sc >= 0.70) {form.GRADE.value="C"}
+	else if (curr_sc >= 0.67) {form.GRADE.value="D+"}
+	else if (curr_sc >= 0.60) {form.GRADE.value="D"}
 	else {form.GRADE.value="F"}
 }
 
