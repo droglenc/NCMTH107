6cefa36f5278abc94a7edd73ef8352d80ba581b4
Derek Ogle <dogle@northland.edu>
2016-10-17 11:48:19 -0500

    Updates to Why Stats is Important CE

--- modules/Why_Statistics_is_Important/CE.R
+++ modules/Why_Statistics_is_Important/CE.R
@@ -24,11 +24,11 @@ Summarize(~age,data=ABCens90,digits=1)
 percTable(xtabs(~sex,data=ABCens90))
 
 set.seed(14787834)
-hist(~replicate(1000,mean(srsdf(ABCens90,50)$age)),xlab="Mean Age",main="",col="gray90",breaks=15)
+hist(~replicate(1000,mean(srsdf(ABCens90,50)$age)),xlab="Mean Age",breaks=15)
 abline(v=mean(ABCens90$age),lwd=2,lty=2)
 tmp <- replicate(1000,percTable(xtabs(~sex,data=srsdf(ABCens90,50)))[["male"]])
 hist(~tmp,xlab="Percent Male",w=2)
 abline(v=percTable(xtabs(~sex,data=ABCens90))[["male"]],lwd=2,lty=2)
 
 
-# Script created at 2016-09-09 13:54:14
+# Script created at 2016-10-17 11:26:19
