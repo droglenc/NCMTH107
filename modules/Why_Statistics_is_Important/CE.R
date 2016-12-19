8591e140ee92c6fc0fc175e8e99e427af04423a7
Derek Ogle <dogle@northland.edu>
2016-12-19 16:16:58 -0600

    Updated Why Stats is Important CE

--- modules/Why_Statistics_is_Important/CE.R
+++ modules/Why_Statistics_is_Important/CE.R
@@ -8,20 +8,18 @@ fnm <- "CE"
 source("../../rhelpers/knitr_setup.R")
 
 library(NCStats)
-data(ABCens90)
 
-data(ABCens90)
-( smp1 <- srsdf(ABCens90,50) )
+( sam1 <- srsdf(ABCens90,n=50) )
 
-Summarize(~age,data=smp1,digits=1)
-percTable(xtabs(~sex,data=smp1))
+Summarize(~age,data=sam1,digits=1)
+percTable(xtabs(~sex,data=sam1),digits=1)
 
-( smp2 <- srsdf(ABCens90,50) )
-Summarize(~age,data=smp2,digits=1)
-percTable(xtabs(~sex,data=smp2))
+( sam2 <- srsdf(ABCens90,n=50) )
+Summarize(~age,data=sam2,digits=1)
+percTable(xtabs(~sex,data=sam2),digits=1)
 
 Summarize(~age,data=ABCens90,digits=1)
-percTable(xtabs(~sex,data=ABCens90))
+percTable(xtabs(~sex,data=ABCens90),digits=1)
 
 set.seed(14787834)
 hist(~replicate(1000,mean(srsdf(ABCens90,50)$age)),xlab="Mean Age",breaks=15)
@@ -31,4 +29,4 @@ hist(~tmp,xlab="Percent Male",w=2)
 abline(v=percTable(xtabs(~sex,data=ABCens90))[["male"]],lwd=2,lty=2)
 
 
-# Script created at 2016-10-17 11:26:19
+# Script created at 2016-12-19 16:16:19
