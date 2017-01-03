c7cb9ddd4b8e23939b4e4a180a9a8fd34590c32d
Derek Ogle <dogle@northland.edu>
2017-01-03 10:00:07 -0600

    Updating why stats exercise

--- modules/Why_Statistics_is_Important/CE.R
+++ modules/Why_Statistics_is_Important/CE.R
@@ -9,24 +9,24 @@ source("../../rhelpers/knitr_setup.R")
 
 library(NCStats)
 
-( sam1 <- srsdf(ABCens90,n=50) )
+( samA <- srsdf(ABCens90,n=100) )
 
-Summarize(~age,data=sam1,digits=1)
-percTable(xtabs(~sex,data=sam1),digits=1)
+Summarize(~age,data=samA,digits=1)
+percTable(xtabs(~sex,data=samA),digits=1)
 
-( sam2 <- srsdf(ABCens90,n=50) )
-Summarize(~age,data=sam2,digits=1)
-percTable(xtabs(~sex,data=sam2),digits=1)
+( samB <- srsdf(ABCens90,n=100) )
+Summarize(~age,data=samB,digits=1)
+percTable(xtabs(~sex,data=samB),digits=1)
 
 Summarize(~age,data=ABCens90,digits=1)
 percTable(xtabs(~sex,data=ABCens90),digits=1)
 
 set.seed(14787834)
-hist(~replicate(1000,mean(srsdf(ABCens90,50)$age)),xlab="Mean Age",breaks=15)
+hist(~replicate(1000,mean(srsdf(ABCens90,100)$age)),xlab="Mean Age",breaks=15)
 abline(v=mean(ABCens90$age),lwd=2,lty=2)
-tmp <- replicate(1000,percTable(xtabs(~sex,data=srsdf(ABCens90,50)))[["male"]])
+tmp <- replicate(1000,percTable(xtabs(~sex,data=srsdf(ABCens90,100)))[["male"]])
 hist(~tmp,xlab="Percent Male",w=2)
 abline(v=percTable(xtabs(~sex,data=ABCens90))[["male"]],lwd=2,lty=2)
 
 
-# Script created at 2016-12-19 16:16:19
+# Script created at 2017-01-03 09:59:17
