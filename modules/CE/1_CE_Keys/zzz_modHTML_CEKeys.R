5a8552fa5d65b1ca5516bfd4f0a04ebe88bff749
Derek Ogle <dogle@northland.edu>
2018-01-11 16:37:33 -0600

    Fixed typos in WhyStats CE Key

--- modules/CE/1_CE_Keys/zzz_modHTML_CEKeys.R
+++ modules/CE/1_CE_Keys/zzz_modHTML_CEKeys.R
@@ -1,4 +1,6 @@
 modHTML_CEKeys <- function(fnm) {
+  ## Save old working directory
+  od <- getwd()
   ## Set working directory
   setwd("C:/aaaWork/Web/GitHub/NCMTH107/modules/CE/1_CE_Keys/")
   ## Load some helper files ... specifically modHTML
@@ -9,4 +11,6 @@ modHTML_CEKeys <- function(fnm) {
   file.rename(from=paste0(fnm,".html"),to=paste0("../",fnm,".html"))
   dir.create(paste0("../",fnm,"_files"),showWarnings=FALSE)
   file.copy(from=paste0(fnm,"_files"),to=paste0("../"),recursive=TRUE)
+  ## Return to old working directory
+  setwd(od)
 }
