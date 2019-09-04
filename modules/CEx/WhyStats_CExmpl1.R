59ca0171a2a30f0eabfc044d3f9677910b2c3e7e
Derek Ogle <dogle@northland.edu>
2019-09-04 14:25:08 -0500

    Updated Why Stats after class

--- modules/CEx/WhyStats_CExmpl1.R
+++ modules/CEx/WhyStats_CExmpl1.R
@@ -19,4 +19,4 @@ hist(~tmp,xlab="Percent Male",w=2)
 abline(v=percTable(xtabs(~sex,data=ABCens90))[["male"]],lwd=2,lty=2)
 
 
-# Script created at 2019-09-03 21:01:12
+# Script created at 2019-09-04 14:23:42
