c01fc5e9764d54cb8e39a14fc2420e298ce60766
Derek Ogle <droglenc@users.noreply.github.com>
2019-11-29 08:28:58 -0600

    Cleaning up

--- Year_Specific/W18/Quiz4.R
+++
@@ -1,4 +0,0 @@
-mc <- read.csv("https://raw.githubusercontent.com/droglenc/NCData/master/MilkCartons.csv")
-levenesTest(dioxin~type,data=mc)
-Summarize(dioxin~type,data=mc)
-( mc.t <- t.test(dioxin~type,data=mc,var.equal=TRUE,alt="less",conf.level=0.99) )
