4a32cf16f594489330c3420b64482e9423033b40
Derek Ogle <droglenc@users.noreply.github.com>
2021-02-27 20:27:22 -0600

    Moved old stuff into zzzOld

--- modules/zzz_Pre_F20/CWD_Script.R
+++
@@ -1,9 +0,0 @@
-library(NCStats)
-setwd("C:/aaaWork/Web/GitHub/NCMTH107/modules/UnivEDA_Quantitative")
-d <- read.csv("CWD.csv")
-str(d)
-hist(~diameter,data=d,xlab="Diameter of CWD")
-Summarize(~diameter,data=d,digits=2)
-
-hist(diameter~exposure,data=d,xlab="Diameter of CWD")
-Summarize(diameter~exposure,data=d,digits=2)
